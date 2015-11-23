d4eaa3a7f16d47d9d947f9bdbfc7d1574675430b
BlackHawk1912 <xblackhawk1912x@gmail.com>
2015-11-23 22:49:31 +0100

    Footer wieder hinzugefügt und Header Nav "gerichtet"

---
+++ php/layout/footer.php
@@ -0,0 +1,5 @@
+	<nav class="navbar navbar-default navbar-sticky-bottom footer">
+		<div class="container">
+			&copy; 2015 Simon von Schnakenburg
+		</div>
+	</nav>
--- php/layout/nav.php
+++ php/layout/nav.php
@@ -1,19 +1,21 @@
-	<nav role="navigation" class="navbar navbar-default navbar-static-top">
-        <div class="navbar-header">
-            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a href="index.php" class="navbar-brand" title="komische Seite">komische Seite</a>
-        </div>
-        <div id="navbarCollapse" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="index.php" title="Startseite">Start</a></li>
-					<li><a href="links.php" title="Links">Links</a></li>
-					<li><a href="studium.php" title="Studium">Studium</a></li>
-					<li><a href="impressum.php" title="Impressum">Impressum</a></li>
-            </ul>
-        </div>
-    </nav>
+<nav role="navigation" class="navbar navbar-default navbar-static-top">
+    <div class="container">
+    <div class="navbar-header">
+        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a href="index.php" class="navbar-brand" title="komische Seite">svns41</a>
+    </div>
+    <div id="navbarCollapse" class="collapse navbar-collapse navbar-right">
+        <ul class="nav navbar-nav">
+            <li><a href="index.php" title="Startseite">Start</a></li>
+            <li><a href="links.php" title="Links">Links</a></li>
+            <li><a href="studium.php" title="Studium">Studium</a></li>
+            <li><a href="impressum.php" title="Impressum">Impressum</a></li>
+        </ul>
+    </div>
+    </div>
+</nav>
