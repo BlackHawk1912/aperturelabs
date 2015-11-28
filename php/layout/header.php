cd6f16d499412d3c62cef289889004d5bb0bee3a
BlackHawk1912 <xblackhawk1912x@gmail.com>
2015-11-28 20:17:38 +0100

    Removed CDN because of problems with mobile devices

--- php/layout/header.php
+++ php/layout/header.php
@@ -2,6 +2,6 @@
 <head>
 	<meta charset="utf-8">
 	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/style.css">
