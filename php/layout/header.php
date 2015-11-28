1d010f0a71ad94e5efe422e1244b4d98be598f71
BlackHawk1912 <xblackhawk1912x@gmail.com>
2015-11-28 20:05:17 +0100

    Fixed problem with CDN of .js

--- php/layout/header.php
+++ php/layout/header.php
@@ -3,8 +3,5 @@
 	<meta charset="utf-8">
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/style.css">
-	<link href="css/bootstrap.css" rel="stylesheet">
-	<link href="js/bootstrap.js" rel="text/javascript">
-	<link rel="stylesheet" href="style.css">
